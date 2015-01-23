8d048aefb0e54284a5bfc36e80cf3c1f58518ab3
Nicolas Genty <nicolas.genty@gmail.com>
2015-01-23 01:55:49 +0100

    add slideshow

---
+++ 00_Macaw-forum-examples/image_slideshow/js/index.js
@@ -0,0 +1,17 @@
+(function() {
+  var $screens = $('.js-screens');
+  var $greenButton = $('.js-green-button');
+  var $orangeButton = $('.js-orange-button');
+  var $redButton = $('.js-red-button');
+
+
+  $redButton.on('click', {index: 0}, showScreen);
+  $orangeButton.on('click', {index: 1}, showScreen);
+  $greenButton.on('click', {index: 2}, showScreen);
+
+  function showScreen(e) {
+    var targetX = -300 * e.data.index;
+    $screens.css('left', targetX);
+  }
+
+})();
